aea3981723e6d6ef454d3b9527c4fe71f549541f
Noel Garces <noel-garces_mbp@Noels-MBP.lan>
2020-09-07 07:16:10 -0600

    first commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from './Product/Product';
+
+const Products = () => (
+  <div>
+    <h1>Products</h1>
+    <Product />
+    <Product />
+    <Product />
+  </div>
+);
+
+export default Products;
